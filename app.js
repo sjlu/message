d8b5bdc5a6a15b236f418d47ec8986aabe4ab9f1
Steven Lu <tacticalazn@gmail.com>
2013-03-14 16:53:12 -0400

    Refactored with mongoose.

--- app.js
+++ app.js
@@ -1,63 +1,44 @@
 var devices = require('./lib/devices');
-var messages = require('./lib/messages');
-var db = require('./lib/database');
+// var messages = require('./lib/messages');
 
 exports.registerDevice = function(type, key, callback)
 {
-	db.getInstance();
-
 	var handler = function(device)
 	{
-		db.destroyInstance();
-		return callback({ id: device.getId() });
+		return callback(device);
 	};
 
-	try {
-		devices.add(type, key, handler);
-	} catch (e) {
-		callback({ error: e.message });
-	}
+	// try {
+		devices.create(type, key, handler);
+	// } catch (e) {
+	// 	callback({ error: e.message });
+	// }
 
 	return;
 };
 
 exports.sendMessage = function(id, message, callback)
 {
-	var m, d;
-	db.getInstance();
-
-	var handler = function()
-	{
-		if (!m || !d)
-			return;
-
-		d.message(m);
-		db.destroyInstance();
-		callback({ id: m.getId() });
-	};
-
 	try {
-		messages.add(message, function(e) {
-			m = e;
-			handler();
-		});
-
-		devices.get(id, function(e) {
-			d = e;
-			handler();
+		devices.get(id, function(device) {
+			device.send(message);
 		});
 	} catch (e) {
 		callback({ error: e.message });
 	}
-
-	return;
 };
 
 exports.registerDevice('ios', '5dc088a8fc317ff6eb1d765856130cb2b4c3dac4f581dad5a501a6da4cfd40d0', function(e)
 {
-	console.log('device: '+JSON.stringify(e));
-	return exports.sendMessage(e.id, 'test', function(e){
+	console.log('new device: '+JSON.stringify(e));
+	exports.sendMessage(e.id, 'test', function(e){
 		return console.log('message: '+JSON.stringify(e));
 	});
+	devices.get(e._id, function(e) {
+		// e.type = 'ios';
+		e.save(function (err, device) {
+			// console.log(err);
+		});
+		console.log('get device: '+JSON.stringify(e));
+	});
 });
-
